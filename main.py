c3f50490ab7a610965f1107d480bbdd1d78c161d
jcom555 <jcom555@aucklanduni.ac.nz>
2021-10-06 01:13:35 +1300

    updated main.py to work correctly

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from pulp import *
 # fetching the names of the stores and their respective demands
 df = pd.read_csv("demandestimations.csv")
 stores = list(df['Store'])
-demand = list(df['Demand Estimation (Pellets)  '])
+# demand = list(df['Demand Estimation (Pellets)  '])
+# demand = [round(num) for num in demand]
 
 # fetching the routes and their respective costs
 df = pd.read_csv("routesWithoutDistribution.csv")
@@ -13,29 +14,32 @@ routes = list(df['route'])
 costs = dict(zip(routes, df['cost']))
 
 # adding a self-titled dictionaries for each store that contains which routes it is visited by
-for store in stores:
-    globals()["%s"%store] = dict(zip(routes, df[store]))
+# for store in stores:
+#     globals()["%s"%store] = dict(zip(routes, df[store]))
 
 # the variables are the number of each routes to take
-routeVars = LpVariable.dicts("Routes", routes, cat='Binary')
+routeVars = LpVariable.dicts("Routes", routes, cat='Integer', lowBound=0, upBound=1)
 
 # adding the minimising cost problem 
 prob  = LpProblem("Minimising_Costs_Problem", LpMinimize)
-prob += lpSum([routeVars[i]*costs[i]] for i in routes)
+prob += lpSum([routeVars[route]*costs[route]] for route in routes)
 
 # the total number of routes <= 60 because there are only 60 trucks
-prob += lpSum(routeVars[i] for i in routes) <= 60
+prob += lpSum(routeVars[route] for route in routes) <= 60
 
-# adding the demand constraints
-i = 0
-for store in stores:
-    prob += lpSum([routeVars[i]*globals()["%s"%store][i]] for i in routes) >= demand[i]
-    i += 1
+for i, store in enumerate(stores):
+    prob += lpSum([routeVars[route] for route in routes if str(i) in route]) == 1 
 
+prob.solve()
 
-# adding the demand requirements for each store
-# for store in stores:
-#     prob += >= demand[i]
+# The status of the solution is printed to the screen
+print("Status:", LpStatus[prob.status])
 
+# Showing the chosen routes
+print("The chosen routes are:")
+for route in routes:
+    if routeVars[route].value() == 1:
+        print(route)
 
-print("hi")
+# The optimised objective function value of Ingredients pue is printed to the screen    
+print("Total Profit from Ties = ", value(prob.objective))
